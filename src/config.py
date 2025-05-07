c8366c751c6ff1275faab6a5a41f60144d74788d
Rushab Munot <rushabmunot1@gmail.com>
2025-05-06 21:32:49 -0400

    Initial commit for setting up the project.

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATA_DIR = os.path.join(BASE_DIR, "data")
+MODEL_DIR = os.path.join(BASE_DIR, "models")
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+NOTEBOOKS = os.path.join(BASE_DIR, "notebooks")
